b1b71115c0babd148f0070cd45542a2ffd56be37
enockabere <maebaenock95@gmail.com>
2021-11-26 13:57:01 +0300

    all changes

--- dashboard/ntl.py
+++ dashboard/ntl.py
@@ -1,10 +1,15 @@
 import requests
 from requests_ntlm import HttpNtlmAuth
+import json
+import random
+import xmltodict
 
-username = "fke-admin"
-password = "Administrator#2021!"
-site_url = "http://102.37.117.22:1447/ADMINBC/WS/FKETEST/Codeunit/MemberPortal/"
-r = requests.get(site_url, auth=HttpNtlmAuth(
-    'domain\\fke-admin', 'Administrator#2021!'))
+session = requests.Session()
+session.auth = HttpNtlmAuth('domain\\fke-admin', 'Administrator#2021!')
+response = session.get(
+    'http://102.37.117.22:1447/ADMINBC/WS/FKETEST/Codeunit/MemberPortal')
 
-print(r.status_code)
+
+dictionary = xmltodict.parse(response.text)
+json_obj = json.dumps(dictionary)
+print(response.text)
