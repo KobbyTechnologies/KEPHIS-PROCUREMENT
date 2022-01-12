2b74e15ca383c735ad2d1780938ecb536af1d7bf
enockabere <maebaenock95@gmail.com>
2022-01-12 15:43:39 +0300

    create post request for restricted tenders

--- tenders/views.py
+++ tenders/views.py
@@ -114,27 +114,45 @@ def Restrict_Details(request, pk):
 
     Access_Point = config.O_DATA.format("/ProcurementMethods")
     Access2 = config.O_DATA.format("/ProcurementRequiredDocs")
+
+    # Responding to Tender
+    vendNo = '01254796'
+    procurementMethod = 1
+    docNo = pk
+    notify = ''
+    if request.method == "POST":
+        unitPrice = int(request.POST.get('amount'))
+
     try:
         r = session.get(Access2, timeout=7).json()
         response = session.get(Access_Point, timeout=9).json()
         Restrict = []
         Doc = []
         for tender in response['value']:
-            if tender['Process_Type'] == 'Tender' and tender['TenderType'] == "Restricted Tender":
+            if tender['Process_Type'] == 'Tender' and tender['TenderType'] == "Restricted Tender" and tender['Status'] == 'New':
                 output_json = json.dumps(tender)
                 Restrict.append(json.loads(output_json))
-                responses = Restrict
-                for my_tender in responses:
+                for my_tender in Restrict:
                     if my_tender['No'] == pk:
-                        res = tender
+                        res = my_tender
         for docs in r['value']:
             if docs['QuoteNo'] == pk:
                 output_json = json.dumps(docs)
                 Doc.append(json.loads(output_json))
-                my_doc = Doc
     except requests.exceptions.ConnectionError as e:
         print(e)
+    try:
+        if vendNo != '' and unitPrice != '':
+            result = config.CLIENT.service.FnCreateProspectiveSupplier(
+                vendNo, procurementMethod, docNo, unitPrice)
+            notify = f"You have successfully Applied for tender number {docNo}"
 
+        else:
+            raise ValueError('Incorrect input!')
+    except Exception as e:
+        warn = f'You have already applied for tender number  {docNo}'
+        print(e)
     todays_date = datetime.datetime.now().strftime("%b. %d, %Y %A")
-    ctx = {"today": todays_date, "res": res, "docs": my_doc}
+    ctx = {"today": todays_date, "res": res,
+           "docs": Doc, "warn": warn, "note": notify}
     return render(request, "details/RES.html", ctx)
