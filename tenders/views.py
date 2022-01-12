9dd95e4d863f7ac3b8a0ee61307e91d67dbbfa2a
enockabere <maebaenock95@gmail.com>
2022-01-12 14:28:06 +0300

    sort reference errors

--- tenders/views.py
+++ tenders/views.py
@@ -20,7 +20,7 @@ def open_tenders(request):
         response = session.get(Access_Point, timeout=10).json()
         open = []
         for tender in response['value']:
-            if tender['Process_Type'] == 'Tender' and tender['TenderType'] == 'Open Tender':
+            if tender['Process_Type'] == 'Tender' and tender['TenderType'] == 'Open Tender' and tender['Status'] == 'New':
                 output_json = json.dumps(tender)
                 open.append(json.loads(output_json))
                 res = open
@@ -45,25 +45,17 @@ def Open_Details(request, pk):
     vendNo = '01254796'
     procurementMethod = 1
     docNo = pk
+    notify = ''
     if request.method == "POST":
         unitPrice = int(request.POST.get('amount'))
-        try:
-            if vendNo != '' and unitPrice != '':
-                result = config.CLIENT.service.FnCreateProspectiveSupplier(
-                    vendNo, procurementMethod, docNo, unitPrice)
-                notify = f"Hi Enock, you have successfully Applied for tender number {docNo}"
-            else:
-                raise ValueError('Incorrect input!')
-        except Exception as e:
-            notify = f'Hi Enock, you have already applied for tender number  {docNo}'
-            print(notify)
+
     try:
         r = session.get(Access2, timeout=7).json()
         response = session.get(Access_Point, timeout=8).json()
         Open = []
         Doc = []
         for tender in response['value']:
-            if tender['Process_Type'] == 'Tender' and tender['TenderType'] == 'Open Tender':
+            if tender['Process_Type'] == 'Tender' and tender['TenderType'] == 'Open Tender' and tender['Status'] == 'New':
                 output_json = json.dumps(tender)
                 Open.append(json.loads(output_json))
                 responses = Open
@@ -74,12 +66,23 @@ def Open_Details(request, pk):
             if docs['QuoteNo'] == pk:
                 output_json = json.dumps(docs)
                 Doc.append(json.loads(output_json))
-                my_doc = Doc
+
     except requests.exceptions.ConnectionError as e:
         print(e)
+    try:
+        if vendNo != '' and unitPrice != '':
+            result = config.CLIENT.service.FnCreateProspectiveSupplier(
+                vendNo, procurementMethod, docNo, unitPrice)
+            notify = f"You have successfully Applied for tender number {docNo}"
 
+        else:
+            raise ValueError('Incorrect input!')
+    except Exception as e:
+        warn = f'You have already applied for tender number  {docNo}'
+        print(e)
     todays_date = datetime.datetime.now().strftime("%b. %d, %Y %A")
-    ctx = {"today": todays_date, "res": res, "docs": my_doc, "note": notify}
+    ctx = {"today": todays_date, "res": res,
+           "docs": Doc, "warn": warn, "note": notify}
     return render(request, "details/open.html", ctx)
 
 
@@ -92,7 +95,7 @@ def Restricted_tenders(request):
         response = session.get(Access_Point, timeout=10).json()
         Restrict = []
         for tender in response['value']:
-            if tender['Process_Type'] == 'Tender' and tender['TenderType'] == "Restricted Tender":
+            if tender['Process_Type'] == 'Tender' and tender['TenderType'] == "Restricted Tender" and tender['Status'] == 'New':
                 output_json = json.dumps(tender)
                 Restrict.append(json.loads(output_json))
                 res = Restrict
