36794451b4a6600312e367d38626700aee61a681
enockabere <maebaenock95@gmail.com>
2022-03-05 20:07:44 +0300

    create signup functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('profile', views.profile_request, name="profile"),
-    path('', views.login_request, name='auth'),
+    path('', views.login_request, name='login'),
+    path('register', views.register_request, name='register'),
+    path('activate/<uidb64>/<token>', views.activate_user, name='activate'),
 ]
